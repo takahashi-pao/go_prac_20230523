be122634c534a9d3679f6e623445c785c6292f3b
takahashi <>
2023-05-23 22:53:32 +0900

    環境構築、実行の確認

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+/* メインメソッド */
 func main() {
 	fmt.Printf("hello, world!")
+	strAndDatetime()
+}
+
+/* 文字列と現在時刻を返す */
+func strAndDatetime() {
+	fmt.Printf("Welcome")
+	fmt.Println("to")
+	fmt.Printf("the playground!")
+
+	fmt.Println("the time is", time.Now())
+
+	var name string = "ジョーギブソン"
+
+	fmt.Println(name)
 }
